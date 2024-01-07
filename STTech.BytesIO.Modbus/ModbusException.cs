6554f6eb963e5e704ffe9f70afb286f391111821
Guijie Lee <landriesnidis@yeah.net>
2024-01-08 05:47:08 +0800

    增加Modbus异常类型

---
+++ STTech.BytesIO.Modbus/ModbusException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace STTech.BytesIO.Modbus
+{
+    public class ModbusException : Exception
+    {
+        public ModbusException(string message) : base(message) { }
+
+        public ModbusException(string message, Exception innerException) : base(message, innerException) { }
+    }
+
+    public class ModbusErrorCodeException : ModbusException
+    {
+        public ModbusErrorCodeException(ModbusErrorCode code) : base(code.GetErrorDescription())
+        {
+            ErrorCode = code;
+        }
+
+        /// <summary>
+        /// 错误码
+        /// </summary>
+        public ModbusErrorCode ErrorCode { get; }
+    }
+}
